22a3b5486ecc5e2c84a72cc7e1402b80525309ae
Bryan Müller <bmuller0975@gmail.com>
2023-08-25 22:51:44 -0500

    feat(build): tweaks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,18 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-import { Card } from "~/components/card/card";
+import {component$} from "@builder.io/qwik";
+import type {DocumentHead} from "@builder.io/qwik-city";
+import {Card} from "~/components/card/card";
 
 export default component$(() => {
   return (
     <>
       <section class="mt-10 flex h-fit flex-col items-center gap-6">
         <Card>
-          <h1 class="font-display border-b-secondary from-primaryDark via-primary to-primaryDark text-primary mt-0 w-fit rounded border-b-4 px-2 text-9xl">
+          <h1 class="font-display border-b-secondary from-primaryDark via-primary to-primaryDark text-primary mt-0 w-fit rounded border-b-4 px-12 text-9xl">
             Sunpunks
           </h1>
-          <p class="font-body mt-10 text-xl text-white">
+        </Card>
+        <Card>
+          <p class="font-body my-5 text-xl text-white">
             The hottest stream team on the interwebs, coming to a screen near
             you!
           </p>
