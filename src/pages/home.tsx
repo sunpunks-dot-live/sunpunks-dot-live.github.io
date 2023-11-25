1787e5ec4377fbde00544eb1bb269a21d7950981
Bryan Muller <bmuller0975@gmail.com>
2023-11-25 00:48:50 -0600

    style: use compressed logo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <article className="font-body p-8 flex flex-col justify-center items-center w-full">
       <img
         alt="Sunpunks Logo"
-        src="logos/sp_fullcolor.png"
+        src="logos/sp_fullcolor-min.png"
         className="max-w-xs mg:max-w-sm"
       />
       <h1 className="font-display text-2xl md:text-3xl lg:text-4xl text-center">
